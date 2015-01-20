5683fd668b76ecce1223188f18b41709c9c591f2
Tomasz Zakrzewski <t.zakrzewski@student.uw.edu.pl>
2015-01-20 17:24:03 +0100

    Refactoring i kolejne usprawnienia wydajnościowe.

--- proj2_336079.py
+++ proj2_336079.py
@@ -13,66 +13,63 @@ def rowne(x, y, eps):
     return abs(x - y) <= eps
 
 
-def newton_fractal(p, x_min, x_max, y_min, y_max, delta, a=1, n=30, c=5, eps=1.0e-15):
+def fraktal(p, x_min, x_max, y_min, y_max, delta, a=1, n=30, c=5, eps=1.0e-15):
     rozm_n = np.int((x_max - x_min) / delta)
     rozm_m = np.int((y_max - y_min) / delta)
-    arr = np.fromfunction(lambda k, l: (x_min + l * delta) + 1j * (y_max - k * delta), (rozm_m, rozm_n))
-    arr = arr.flatten()
-    q = deque()
-    q.append(arr.copy())
-    fp = p.deriv(1)
-    counts = np.zeros(shape=arr.shape)
-    unconverged = np.ones(shape=arr.shape, dtype=bool)
-    indices = np.arange(len(arr))
+    punkty = np.fromfunction(lambda k, l: (x_min + l * delta) + 1j * (y_max - k * delta), (rozm_m, rozm_n))
+    punkty = punkty.flatten()
+    kol = deque()
+    kol.append(punkty.copy())
+    pochodna = p.deriv(1)
+    ostatnia_iteracja = np.zeros(shape=punkty.shape)  # Numer iteracji po której osiągnęliśmy zbieżność/cykliczność.
+    niezbiezne = np.ones(shape=punkty.shape, dtype=bool)  # Prawda dla każdego punktu, który nie jest jeszcze zbieżny.
+    wynik = np.zeros(shape=punkty.shape, dtype=int)
+    indeksy = np.arange(len(punkty))
     for i in range(n):
-        f_g = p(arr[unconverged])
-        new_unconverged = np.abs(f_g) > eps
-        counts[indices[unconverged][~new_unconverged]] = i
-        unconverged[unconverged] = new_unconverged
-        arr[unconverged] -= a * f_g[new_unconverged] / fp(arr[unconverged])
+        wartosci = p(punkty[niezbiezne])
+        nowe_niezbiezne = np.abs(wartosci) > eps
+        ostatnia_iteracja[indeksy[niezbiezne][~nowe_niezbiezne]] = i
+        niezbiezne[niezbiezne] = nowe_niezbiezne
+        punkty[niezbiezne] -= a * wartosci[nowe_niezbiezne] / pochodna(punkty[niezbiezne])
 
-        id_cyklu = i - len(q) + 1
-        for poprz in q:
-            cykle = rowne(arr, poprz, eps)[unconverged]
-            counts[indices[unconverged][cykle]] = id_cyklu
-            unconverged[unconverged][cykle] = False
+        id_cyklu = i - len(kol) + 1
+        for poprz in kol:
+            cykle = rowne(punkty, poprz, eps)[niezbiezne]
+            ostatnia_iteracja[indeksy[niezbiezne][cykle]] = id_cyklu
+            niezbiezne[niezbiezne][cykle] = False
             id_cyklu = id_cyklu + 1
+            # TODO średnia?
 
-        if len(q) + 1 > c:
-            q.popleft()
-        q.append(arr.copy())
-
-    arr = arr.reshape((rozm_m, rozm_n))
-    counts = counts.reshape((rozm_m, rozm_n))
-    unconverged = unconverged.reshape((rozm_m, rozm_n))
-
-    wynik = np.zeros(shape=counts.shape, dtype=int)
+        if len(kol) + 1 > c:
+            kol.popleft()
+        kol.append(punkty.copy())
 
     ostatni_nr = 1
     s = set()
-    for k, l in product(range(rozm_m), range(rozm_n)):
+    zbiezne = punkty[~niezbiezne]
+    zbiezne_indeksy = indeksy[~niezbiezne]
+    for i in range(len(zbiezne)):
+        obecny = False
         numer = 0
-        if unconverged[k, l] != 1:
-            obecny = False
+        for pkt, nr in s:
+            if rowne(pkt, zbiezne[i], eps):
+                obecny = True
+                numer = nr
+                break
+
+        if not obecny:
+            s.add((zbiezne[i], ostatni_nr + 1))
+            ostatni_nr += 1
             numer = ostatni_nr
-            for pkt, nr in s:
-                if rowne(pkt, arr[k, l], eps):
-                    obecny = True
-                    numer = nr
-                    break
 
-            if not obecny:
-                s.add((arr[k, l], ostatni_nr + 1))
-                ostatni_nr += 1
-                numer = ostatni_nr
+        wynik[zbiezne_indeksy[i]] = ostatnia_iteracja[zbiezne_indeksy[i]] + numer * n
 
-        wynik[k, l] = counts[k, l] + numer * n
-    return wynik
+    return wynik.reshape((rozm_m, rozm_n))
 
 
 w = Polynomial([-1, 0, 0, 1]) #z^3 - 1
 begin = time.time()
-pic = newton_fractal(w, -1, 1, -1, 1, 0.003, a=1.0 + 0.1j, n=30)
+pic = fraktal(w, -1, 1, -1, 1, 0.003, a=1.0 + 0.1j, n=30)
 print(time.time() - begin)
 plt.figure(figsize=(8, 8))
 plt.imshow(pic)
