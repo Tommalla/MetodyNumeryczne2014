f0a5f57192845c260c974c7c54b48eee311613dd
Tomasz Zakrzewski <t.zakrzewski@student.uw.edu.pl>
2015-01-20 17:03:35 +0100

    Mocno zoptymalizowana wersja drugiego projektu.
    Potrzeba tutaj unormować nazwy zmiennych.

--- proj2_336079.py
+++ proj2_336079.py
@@ -1,8 +1,9 @@
-import numpy as np
-import matplotlib.pyplot as plt
 from numpy.polynomial.polynomial import Polynomial
 from itertools import product
+import numpy as np
+import matplotlib.pyplot as plt
 from collections import deque
+import time
 
 
 kto = 'Tomasz Zakrzewski'
@@ -12,60 +13,67 @@ def rowne(x, y, eps):
     return abs(x - y) <= eps
 
 
-def fraktal(p, x_min, x_max, y_min, y_max, delta, a=1, n=30, c=5, eps=1.0e-15):
+def newton_fractal(p, x_min, x_max, y_min, y_max, delta, a=1, n=30, c=5, eps=1.0e-15):
     rozm_n = np.int((x_max - x_min) / delta)
     rozm_m = np.int((y_max - y_min) / delta)
+    arr = np.fromfunction(lambda k, l: (x_min + l * delta) + 1j * (y_max - k * delta), (rozm_m, rozm_n))
+    arr = arr.flatten()
     q = deque()
-    zera = p.roots()
-    pochodna = p.deriv(1)
-    punkty = np.fromfunction(lambda k, l: (x_min + l * delta) + 1j * (y_max - k * delta), (rozm_m, rozm_n))
-    iteracja_koniec = np.zeros((rozm_m, rozm_n), dtype=np.int)
-    pierwiastek = np.zeros((rozm_m, rozm_n), dtype=np.complex)
-    nr_pierwiastka = np.zeros((rozm_m, rozm_n), dtype=np.int)
-    q.append(punkty.copy())
+    q.append(arr.copy())
+    fp = p.deriv(1)
+    counts = np.zeros(shape=arr.shape)
+    unconverged = np.ones(shape=arr.shape, dtype=bool)
+    indices = np.arange(len(arr))
     for i in range(n):
-        punkty = punkty - a * p(punkty) / pochodna(punkty)
-        # Tutaj zamień kolejność pętli...
-        for k, l in product(range(rozm_m), range(rozm_n)):
-            if iteracja_koniec[k, l] == 0:
-                pkt = punkty[k, l]
-                id_cyklu = i - len(q) + 1
-                for poprz in q:
-                    if rowne(pkt, poprz[k, l], eps):
-                        pierwiastek[k, l] = (pkt + poprz[k, l]) / 2
-                        iteracja_koniec[k, l] = id_cyklu
-                    id_cyklu += 1
+        f_g = p(arr[unconverged])
+        new_unconverged = np.abs(f_g) > eps
+        counts[indices[unconverged][~new_unconverged]] = i
+        unconverged[unconverged] = new_unconverged
+        arr[unconverged] -= a * f_g[new_unconverged] / fp(arr[unconverged])
+
+        id_cyklu = i - len(q) + 1
+        for poprz in q:
+            cykle = rowne(arr, poprz, eps)[unconverged]
+            counts[indices[unconverged][cykle]] = id_cyklu
+            unconverged[unconverged][cykle] = False
+            id_cyklu = id_cyklu + 1
 
         if len(q) + 1 > c:
             q.popleft()
-        q.append(punkty.copy())
+        q.append(arr.copy())
 
+    arr = arr.reshape((rozm_m, rozm_n))
+    counts = counts.reshape((rozm_m, rozm_n))
+    unconverged = unconverged.reshape((rozm_m, rozm_n))
+
+    wynik = np.zeros(shape=counts.shape, dtype=int)
+
+    ostatni_nr = 1
     s = set()
-    ostatni_nr = 0
     for k, l in product(range(rozm_m), range(rozm_n)):
-        if iteracja_koniec[k, l] != 0:
+        numer = 0
+        if unconverged[k, l] != 1:
             obecny = False
             numer = ostatni_nr
             for pkt, nr in s:
-                if rowne(pkt, punkty[k, l], eps):
+                if rowne(pkt, arr[k, l], eps):
                     obecny = True
                     numer = nr
                     break
 
             if not obecny:
-                s.add((punkty[k, l], ostatni_nr + 1))
+                s.add((arr[k, l], ostatni_nr + 1))
                 ostatni_nr += 1
                 numer = ostatni_nr
 
-            nr_pierwiastka[k, l] = numer
-
-    mapa = np.zeros((rozm_m, rozm_n), dtype=np.int)
-    for k, l in product(range(rozm_m), range(rozm_n)):
-        mapa[k,l] = nr_pierwiastka[k, l] * n + iteracja_koniec[k, l]
-    return mapa, punkty
+        wynik[k, l] = counts[k, l] + numer * n
+    return wynik
 
 
 w = Polynomial([-1, 0, 0, 1]) #z^3 - 1
-f, wyniki = fraktal(w, -1, 1, -1, 1, 0.003, a=1.0+0.2j, n=30, c=5)
+begin = time.time()
+pic = newton_fractal(w, -1, 1, -1, 1, 0.003, a=1.0 + 0.1j, n=30)
+print(time.time() - begin)
 plt.figure(figsize=(8, 8))
-plt.imshow(f)
+plt.imshow(pic)
+plt.show()
