51f15f84f4135a534adee1d6041c6c655ca968f4
Tomasz Zakrzewski <t.zakrzewski@student.uw.edu.pl>
2015-01-20 22:53:39 +0100

    Poprawiony błąd z wykrywaniem cykli, poprawna graficzna reprezentacja cykli oraz przypadkowe przyspieszenie.

--- proj2_336079.py
+++ proj2_336079.py
@@ -1,10 +1,10 @@
-from collections import deque
 import time
+from collections import deque
 
-from IPython.display import display, Math
 import numpy as np
-from numpy.polynomial.polynomial import Polynomial
 import matplotlib.pyplot as plt
+from numpy.polynomial.polynomial import Polynomial
+from IPython.display import display, Math
 
 
 kto = 'Tomasz Zakrzewski'
@@ -30,6 +30,7 @@ def fraktal(p, x_min, x_max, y_min, y_max, delta, a=1, n=30, c=5, eps=1.0e-15):
     Funkcja zwraca tablicę liczbową (int lub float) m, w której zakodowana jest klasyfikacja ciągu iteracji:
     dla każdego punktu startowego: m[k, l] określa zachowanie punktu x_min + l * delta + 1j * y_max - k * delta
     """
+    max_pierw = len(p.roots())
     rozm_n = np.int((x_max - x_min) / delta)
     rozm_m = np.int((y_max - y_min) / delta)
     punkty = np.fromfunction(lambda k, l: (x_min + l * delta) + 1j * (y_max - k * delta), (rozm_m, rozm_n))
@@ -37,8 +38,9 @@ def fraktal(p, x_min, x_max, y_min, y_max, delta, a=1, n=30, c=5, eps=1.0e-15):
     kol = deque()
     kol.append(punkty.copy())
     pochodna = p.deriv(1)
-    ostatnia_iteracja = np.zeros(shape=punkty.shape)  # Numer iteracji po której osiągnęliśmy zbieżność/cykliczność.
+    ostatnia_iteracja = np.zeros(shape=punkty.shape, dtype=int)  # Numer iteracji po której osiągnęliśmy zbieżność/cykliczność.
     niezbiezne = np.ones(shape=punkty.shape, dtype=bool)  # Prawda dla każdego punktu, który nie jest jeszcze zbieżny.
+    jest_cykl = np.zeros(shape=punkty.shape, dtype=bool)
     wynik = np.zeros(shape=punkty.shape, dtype=int)
     indeksy = np.arange(len(punkty))
     for i in range(n):
@@ -48,36 +50,41 @@ def fraktal(p, x_min, x_max, y_min, y_max, delta, a=1, n=30, c=5, eps=1.0e-15):
         niezbiezne[niezbiezne] = nowe_niezbiezne
         punkty[niezbiezne] -= a * wartosci[nowe_niezbiezne] / pochodna(punkty[niezbiezne])
 
-        id_cyklu = i - len(kol) + 1
-        for poprz in kol:
-            cykle = rowne(punkty, poprz, eps)[niezbiezne]
-            ostatnia_iteracja[indeksy[niezbiezne][cykle]] = id_cyklu
-            niezbiezne[niezbiezne][cykle] = False
-            id_cyklu = id_cyklu + 1
-
         if len(kol) + 1 > c:
             kol.popleft()
         kol.append(punkty.copy())
 
+    for i in range(2):  # Pozbywamy się cykli dł. 0 i 1
+        if len(kol) > 0:
+            kol.pop()
+    for k in range(len(kol)):
+        poprz = kol[k]
+        for j in range(len(poprz)):
+            if niezbiezne[j] and rowne(punkty[j], poprz[j], eps):
+                jest_cykl[j] = 1
+                niezbiezne[j] = 0
+
     ostatni_nr = 1
     s = set()
-    zbiezne = punkty[~niezbiezne]
-    zbiezne_indeksy = indeksy[~niezbiezne]
-    for i in range(len(zbiezne)):
-        obecny = False
-        numer = 0
-        for pkt, nr in s:
-            if rowne(pkt, zbiezne[i], eps):
-                obecny = True
-                numer = nr
-                break
+    for i in range(len(punkty)):
+        if not niezbiezne[i]:
+            obecny = False
+            numer = 0
+            for pkt, nr in s:
+                if rowne(pkt, punkty[i], eps):
+                    obecny = True
+                    numer = nr
+                    break
 
-        if not obecny:
-            s.add((zbiezne[i], ostatni_nr + 1))
-            ostatni_nr += 1
-            numer = ostatni_nr
+            if not obecny:
+                s.add((punkty[i], ostatni_nr + 1))
+                ostatni_nr += 1
+                numer = ostatni_nr
 
-        wynik[zbiezne_indeksy[i]] = ostatnia_iteracja[zbiezne_indeksy[i]] + numer * n
+            if jest_cykl[i]:
+                wynik[i] = (numer + max_pierw) * n
+            else:
+                wynik[i] = ostatnia_iteracja[i] + numer * n
 
     return wynik.reshape((rozm_m, rozm_n))
 
@@ -96,7 +103,7 @@ def demo():
     plt.ion()
     print('Każdy wielomian po 30 iteracji, każdy na przedziale -1 - i do 1 + i, delta = 0.003')
     dane = [
-        ([-1, 0, 0, 1], 1.0, 5, 1e-15), # Wielomian, a, c
+        ([-1, 0, 0, 1], 1.0, 0, 1e-15), # Wielomian, a, c
         ([-1, 0, 0, 1], 1.0 + 0.1j, 5, 1e-15),
         ([-16, 0, 0, 0, 15, 0, 0, 0, 1], 1.0, 7, 1e-10),
         ([-16, 0, 0, 0, 15, 0, 0, 0, 1], 1.1 + 0.2j, 14, 1e-10),
@@ -111,6 +118,3 @@ def demo():
         plt.figure(figsize=(8, 8))
         plt.imshow(wynik)
         plt.show()
-
-
-demo()
